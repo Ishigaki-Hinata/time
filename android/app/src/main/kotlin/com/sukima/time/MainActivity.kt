c907b7d6345e15567c7a39ae272c49d89a67ca65
hinut <si19003@edu.aomori-u.ac.jp>
2023-01-06 20:26:28 +0900

    Initial commit

---
+++ android/app/src/main/kotlin/com/sukima/time/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.sukima.time
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Baby Names',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() {
+    return _MyHomePageState();
+  }
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<DocumentSnapshot> documentList = [];
+  // List<String> name = [];
+  // List<int> votes = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Baby Name Votes')),
+      body: FutureBuilder(
+        future: initialize(),
+        builder: (context, snapshot) {
+          // // 通信中はスピナーを表示
+          // if (snapshot.connectionState != ConnectionState.done) {
+          //   return CircularProgressIndicator();
+          // }
+
+          // // エラー発生時はエラーメッセージを表示
+          // if (snapshot.hasError) {
+          //   return Text(snapshot.error.toString());
+          // }
+
+          // // データがnullでないかチェック
+          // if (!snapshot.hasData) {
+          //   return Text("データが存在しません");
+          // }
+
+          // documentList.forEach((elem) {
+          //   name.add(elem.get('name'));
+          //   votes.add(elem.get('votes'));
+          // });
+          // return Column(
+          //   children: <Widget> [
+          //     Text(name[0] + ':' + votes[0].toString()),
+          //     Text(name[1] + ':' + votes[1].toString()),
+          //   ],
+
+          return Column(
+            children: documentList.map((data) => Text(data.get('name') + ' : ' + data.get('votes').toString())).toList(),
+          );
+        },
+      ),
+    );
+  }
+
+  Future<void> initialize() async {
+    final snapshot =
+    await FirebaseFirestore.instance.collection('baby').get();
+    documentList = snapshot.docs;
+
+    print("##################################################### initialize()");
+    documentList.forEach((elem) {
+      print(elem.get('name'));
+      print(elem.get('votes'));
+    });
+    print("##################################################### initialize()");
+  }
+}
