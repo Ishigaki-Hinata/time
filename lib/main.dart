e92131b2026d71802a6d40bfe30eb7123d15cd77
hinut <si19003@edu.aomori-u.ac.jp>
2023-01-06 22:54:05 +0900

    StreamBuilderに変更＋補足

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,26 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
+  //アプリ実行前にFlutterアプリの機能を利用する場合に宣言(初期化のような動き)
   WidgetsFlutterBinding.ensureInitialized();
+  //Firebaseのパッケージを呼び出し
+
+  //await ・・・非同期処理が完了するまで待ち、その非同期処理の結果を取り出してくれる
+  //awaitを付与したら asyncも付与する
   await Firebase.initializeApp();
   runApp(MyApp());
 }
+
+//Stateless ・・・状態を保持する（動的に変化しない）
+// Stateful  ・・・状態を保持しない（変化する）
+// overrride ・・・上書き
+
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // アイコンやタスクバーの時の表示
       title: 'Baby Names',
       home: MyHomePage(),
     );
@@ -20,22 +31,28 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   @override
+  //createState()でState（Stateを継承したクラス）を返す
   _MyHomePageState createState() {
     return _MyHomePageState();
   }
 }
 
+//Stateをextendsしたクラスを作る
 class _MyHomePageState extends State<MyHomePage> {
+  //ドキュメント情報を入れる箱を用意
   List<DocumentSnapshot> documentList = [];
   // List<String> name = [];
   // List<int> votes = [];
 
   @override
   Widget build(BuildContext context) {
+    //デザインWidget
     return Scaffold(
       appBar: AppBar(title: Text('Baby Name Votes')),
-      body: FutureBuilder(
-        future: initialize(),
+      //非同期処理でWigetを生成
+      body: StreamBuilder(
+        //initialize ・・・初期化
+        //future: initialize(),
         builder: (context, snapshot) {
           // // 通信中はスピナーを表示
           // if (snapshot.connectionState != ConnectionState.done) {
@@ -63,7 +80,11 @@ class _MyHomePageState extends State<MyHomePage> {
           //   ],
 
           return Column(
-            children: documentList.map((data) => Text(data.get('name') + ' : ' + data.get('votes').toString())).toList(),
+
+            // map ・・・要素それぞれに対して、渡した関数の処理を加えて新しく繰り返し処理する
+            // データを取得（名前と数）してテキストとしてColumnに書き出す
+
+          children: documentList.map((data) => Text(data.get('name') + ' : ' + data.get('votes').toString())).toList(),
           );
         },
       ),
